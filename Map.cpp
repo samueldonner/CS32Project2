2d8c1f0875e9a3e1e6b63caf0db8004b408e86be
samueldonner <samueldonner@Samuels-MacBook-Pro.local>
2016-01-25 21:24:48 -0800

    Finished

--- Map.cpp
+++ Map.cpp
@@ -11,7 +11,7 @@ bool combine(const Map& m1, const Map& m2, Map& result)
     KeyType keyM2;
     ValueType valueM2;
     bool passOrFail = true;
-    Map resultTemp;
+    Map resultTemp; // create temp result variable
     for(int i = 0; i < m1.size(); i++)
     {
         for( int j = 0; j < m2.size(); j++ )
@@ -20,7 +20,7 @@ bool combine(const Map& m1, const Map& m2, Map& result)
             m2.get(j, keyM2, valueM2);
             resultTemp.insert(keyM1, valueM1);
             resultTemp.insert(keyM2, valueM2);
-        }
+        } //insert all keys from m1 and m2 (duplicates will not be inserted)
     }
     
     for(int i = 0; i < m1.size(); i++)
@@ -33,7 +33,8 @@ bool combine(const Map& m1, const Map& m2, Map& result)
             {
                 resultTemp.erase(keyM1);
                 passOrFail = false;
-            }
+            } // if m1 and m2 keys equal each other and the values dont equal each other
+              // remove the that key from the resultTemp Map and set passOrFail = to false
         }
     }
     
@@ -42,42 +43,83 @@ bool combine(const Map& m1, const Map& m2, Map& result)
     return passOrFail;
 }
 
+
 void subtract(const Map& m1, const Map& m2, Map& result)
 {
+    KeyType keyM1;
+    ValueType valueM1;
+    KeyType keyM2;
+    ValueType valueM2;
+    Map resultTemp;
+    for( int j = 0; j < m2.size(); j++ )
+    {
+        m1.get(j, keyM1, valueM1);
+        resultTemp.insert(keyM1, valueM1);
+    } // insert all m1 keys into resultTemp
     
+    for(int j = 0; j < m2.size(); j++)
+    {
+        for( int i = 0; i < resultTemp.size(); i++ )
+        {
+            resultTemp.get(i, keyM1, valueM1);
+            m2.get(j, keyM2, valueM2);
+            if (keyM1==keyM2)
+            {
+                resultTemp.erase(keyM1);
+            } // if keym1 and keym2 are equal erase keym1 from result map
+        } // go through m2 and result temp and compaure each function
+    }
+    
+    result = resultTemp;
 }
 
 
 Map::Map()
 {
     head = nullptr;
-}
+} // construct map and set head = to nullptr
 
 
 Map::Map(const Map& other)
 {
     Node *pOther = other.head;
+    while( pOther!=nullptr )
+    {
+        if(pOther->next==nullptr)
+            break;
+        pOther = pOther->next;
+    } // go to end of the other Map
+    
     while( pOther!=nullptr)
     {
         insert(pOther->key, pOther->value);
-        pOther = pOther->next;
-    }
+        pOther = pOther->previous;
+    } // go backwards through other Map and insert
+      // keys in right order
 }
 
 Map& Map::operator=(const Map& otherSide)
 {
-    Node *p = head;
     Node *pOtherSide = otherSide.head;
+    Node *p=head;
     while( p!=nullptr )
     {
         erase(p->key);
         p = p->next;
-    }
+    } // erase this Map
+    while( pOtherSide!=nullptr )
+    {
+        if(pOtherSide->next==nullptr)
+             break;
+        pOtherSide = pOtherSide->next;
+    } // go to end of otherSide map
+
     while( pOtherSide!=nullptr)
     {
         insert(pOtherSide->key, pOtherSide->value);
-        pOtherSide = pOtherSide->next;
-    }
+        pOtherSide = pOtherSide->previous;
+    } // go backwards through other Map and insert
+    // keys in right order
     return (*this);
 }
 
@@ -86,7 +128,7 @@ Map::~Map()
     if(head == nullptr)
     {
         return;
-    }
+    } // check if Map is empty
     Node *p = head;
     while(p!=nullptr)
     {
@@ -94,7 +136,7 @@ Map::~Map()
         Node* killer = p;
         p = p->next;
         delete killer;
-    }
+    } //delete all Nodes in Map
 }
 
 bool Map::empty() const
@@ -103,7 +145,7 @@ bool Map::empty() const
         return true;
     else
         return false;
-}
+} // check if Map is empty
 
 int Map::size() const
 {
@@ -113,7 +155,7 @@ int Map::size() const
     {
         sizeCounter++;
         p = p->next;
-    }
+    } // increase size Counter while cycling through Map
     
     return sizeCounter;
 }
@@ -121,16 +163,16 @@ int Map::size() const
 bool Map::insert(const KeyType& key, const ValueType& value)
 {
     if( contains(key) )
-        return false;
+        return false; // dont insert if key is contained
     Node *p = new Node;
     p->value = value;
     p->key = key;
     p->next = head;
-    p->previous = nullptr;
+    p->previous = nullptr; //linke up items
     if(head != nullptr)
     {
         head->previous = p;
-    }
+    } // make sure Map is not empty
     head = p;
     
     return true;
@@ -145,9 +187,9 @@ bool Map::update(const KeyType& key, const ValueType& value)
         {
             p->value = value;
             return true;
-        }
+        } // if Map key is key then Map value equals
         p = p->next;
-    }
+    } // go through Map
     return false;
 }
 
@@ -156,11 +198,11 @@ bool Map::insertOrUpdate(const KeyType& key, const ValueType& value)
     if(contains(key))
     {
         update(key, value);
-    }
+    } // if map contains update it
     else
     {
         insert(key, value);
-    }
+    } // otherwise insert the key
     return true;
 }
 
@@ -170,7 +212,7 @@ bool Map::erase(const KeyType& key)
     while( p!=nullptr )
     {
         if(p->key == key)
-        {
+        { // if Map key equals key
             Node *killer = p;
             Node *temp = killer->next;
             if( p->previous == nullptr )
@@ -189,12 +231,12 @@ bool Map::erase(const KeyType& key)
             if(temp!=nullptr)
             {
                 temp->previous = p;
-            }
+            } // if temp is empty
             delete killer;
             return true;
         }
         p = p->next;
-    }
+    } // go through Map
     
     return false;
 }
@@ -203,10 +245,9 @@ bool Map::contains(const KeyType& key) const
 {
     Node *p = head;
     while( p!=nullptr )
-    {
+    { // go thorugh Map
         if( p->key == key )
-        {
-            
+        { // if key is in Map return true
             return true;
         }
         p = p->next;
@@ -222,17 +263,17 @@ void Map::dump()
         cout << "Name: " << p->key << "|   Value: " << p->value << endl;
         p = p->next;
     }
-}
+} //print Map
 
 bool Map::get(const KeyType& key, ValueType& value) const
 {
     Node *p = head;
     while( p!=nullptr )
-    {
+    { // go through map
         if( p->key == key )
-        {
-            value = p->value;
-            return true;
+        { // Map key == key
+            value = p->value; // set value = to Map value
+            return true; //return true if key is in Map
         }
         p = p->next;
     }
@@ -244,9 +285,9 @@ bool Map::get(int i, KeyType& key, ValueType& value) const
     Node *p = head;
     int getIndex = 0;
     while( p!=nullptr )
-    {
+    { // go through Map
         if( getIndex == i )
-        {
+        { // if the index equals I then set key and value to the Map's
             key = p->key;
             value =p->value;
             return true;
@@ -259,9 +300,9 @@ bool Map::get(int i, KeyType& key, ValueType& value) const
 
 void Map::swap(Map& other)
 {
-    Map tempMap;
+    Map tempMap; //create tempoarty map
     tempMap = other;
     other = *this;
     *this = tempMap;
-}
+} //swamp maps
 
--- main.cpp
+++ main.cpp
@@ -11,28 +11,28 @@ using namespace std;
 
 int main()
 {
-    Map map1;
+    /*Map map1;
     Map map2;
     
     map1.insert("Fred", 123);
     map1.insert("Ethel", 456);
     map1.insert("Lucy", 789);
     
-    map2.insert("Lucy", 654);
+    map2.insert("Lucy", 789);
     map2.insert("Ricky", 321);
+    map2.insert("Ethel", 654);
     cout<<endl;
     map1.dump();
     cout<<endl;
     map2.dump();
     cout<<endl;
-    cout<<combine(map1, map2, map2);
+    subtract(map1, map2, map2);
     map2.dump();
     cout<<endl;
     
-    //combine(map1, map2, map3);
-    /*Map map;
+    //combine(map1, map2, map3);*/
+    Map map;
     Map map2;
-    
     
     if(map.empty())
         cout << "Map is empty" << endl;
@@ -47,22 +47,28 @@ int main()
     map2.insert("Sal", 454536);
     map2.insert("Rick", 56423);
     
+    cout<< "MAP:" << endl;
     map.dump();
     cout << endl;
+    cout<< "MAP2:" << endl;
     map2.dump();
     cout << endl;
-    map.swap(map2);
-    map.dump();
+    //map.swap(map2);
+    Map map3(map2);
+    cout<< "MAP3:" << endl;
+    map3.dump();
     cout << endl;
+    cout<< "MAP2:" << endl;
     map2.dump();
     cout << endl;
     
-    Map map3(map2);
+    
+    
+    /*Map map3(map2);
     cout << "map 3: " << endl;
     map3.dump();
-     */
     
-    /*cout << "Map has " << map.size() << " elements." << endl;
+    cout << "Map has " << map.size() << " elements." << endl;
     
     //map.erase("Sam");
     //map.erase("Joe");
